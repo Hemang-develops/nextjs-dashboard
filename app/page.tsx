ec52bcb4ec84034d9313fef91de95deec8f269a8
Hemang Patel <136294289+Hemang-develops@users.noreply.github.com>
2025-06-29 05:28:52 +0000

    till chap 11

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import AcmeLogo from "@/app/ui/acme-logo";
-import { ArrowRightIcon } from "@heroicons/react/24/outline";
+import {
+  ArrowRightIcon,
+  ComputerDesktopIcon,
+} from "@heroicons/react/24/outline";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -19,12 +22,21 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
+          <div className="flex gap-3">
+            <Link
+              href="/login"
+              className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            >
+              <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
+            </Link>
+            <Link
+              href="/dashboard"
+              className="flex items-center gap-5 self-start rounded-lg border border-blue-500 px-6 py-3 text-sm font-medium text-blue-500 transition-colors hover:bg-blue-100 md:text-base"
+            >
+              <span>Dashboard</span>{" "}
+              <ComputerDesktopIcon className="w-5 md:w-6" />
+            </Link>
+          </div>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
